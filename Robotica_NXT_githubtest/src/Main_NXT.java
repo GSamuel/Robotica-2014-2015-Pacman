8b835ba2b2d5af1ae1a49a65f92bb650a58d9a98
GSamuel <gideon_hoog@hotmail.com>
2014-09-02 12:54:38 +0200

    test projecten printen "test" op het scherm

---
+++ Robotica_NXT_githubtest/src/Main_NXT.java
@@ -0,0 +1,14 @@
+
+public class Main_NXT {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Robotica_PC_githubtest/src/Main_PC.java
@@ -0,0 +1,9 @@
+
+public class Main_PC {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+
+	}
+
+}
