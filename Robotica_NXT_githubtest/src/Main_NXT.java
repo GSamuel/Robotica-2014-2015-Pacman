00bee8fc31b3a3c5c837c05367e878e3bb3cc1a6
GSamuel <gideon_hoog@hotmail.com>
2014-09-04 11:32:04 +0200

    format changed

--- Robotica_NXT_githubtest/src/Main_NXT.java
+++ Robotica_NXT_githubtest/src/Main_NXT.java
@@ -1,17 +1,18 @@
 import lejos.nxt.Button;
 
+public class Main_NXT
+{
 
-public class Main_NXT {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("test");
 		System.out.println("druk op een knop");
-		
+
 		Button.waitForAnyPress();
-		
+
 		System.out.println("knop ingedrukt");
 		System.out.println("druk op een knop");
-		
+
 		Button.waitForAnyPress();
 
 	}
--- Robotica_PC_githubtest/src/Main_PC.java
+++ Robotica_PC_githubtest/src/Main_PC.java
@@ -1,7 +1,8 @@
+public class Main_PC
+{
 
-public class Main_PC {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("test");
 
 	}
