82252fb986694f8a8f5d649399fc1712c73f831a
GSamuel <gideon_hoog@hotmail.com>
2014-09-08 18:39:07 +0200

    Communication test

--- Robotica_NXT_githubtest/src/CommTest.java
+++ Robotica_NXT_githubtest/src/CommTest.java
@@ -1,9 +1,10 @@
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
 import lejos.nxt.Button;
-import lejos.nxt.LCD;
-import lejos.nxt.comm.Bluetooth;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
 import lejos.nxt.comm.NXTConnection;
 import lejos.nxt.comm.USB;
 
@@ -11,27 +12,44 @@ public class CommTest
 {
 	public static void main(String[] args)
 	{
-		LCD.drawString("right BT", 0, 0);
+		TouchSensor touch1 = new TouchSensor(SensorPort.S1);
+		TouchSensor touch2 = new TouchSensor(SensorPort.S2);
+
 		NXTConnection connection = null;
 
-		if (Button.waitForAnyPress() == Button.ID_RIGHT)
+		do
 		{
-			LCD.drawString("waiting for BT", 0, 1);
-			connection = Bluetooth.waitForConnection();
-		} else
-		{
-			LCD.drawString("waiting for USB", 0, 1);
+			System.out.println("waiting for USB");
 			connection = USB.waitForConnection();
-		}
 
-		DataOutputStream dataOut = connection.openDataOutputStream();
-		try
-		{
-			dataOut.writeInt(1234);
-		} catch (IOException e)
-		{
-			System.out.println(" write error " + e);
-		}
+			DataOutputStream dataOut = connection.openDataOutputStream();
+			DataInputStream dataIn = connection.openDataInputStream();
+
+			try
+			{
+
+				while (!touch1.isPressed())
+				{
+					dataOut.writeInt(1234);
+					dataOut.flush();
+					Thread.sleep(100);
+
+				}
+				connection.close();
+				System.out.println("close connection");
+			} catch (IOException e)
+			{
+				System.out.println(" write error " + e);
+			} catch (InterruptedException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("touch2:"+touch2.isPressed());
+		} while (touch2.isPressed());
+
+		Button.waitForAnyPress();
+
 	}
 
 }
