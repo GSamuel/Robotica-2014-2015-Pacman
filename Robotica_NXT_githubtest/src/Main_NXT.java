8caf429aa6eb1c0b76df5c41f1b7cc8023fe56b3
GSamuel <gideon_hoog@hotmail.com>
2014-09-08 17:24:05 +0200

    CommTest

--- Robotica_NXT_githubtest/src/Main_NXT.java
+++ Robotica_NXT_githubtest/src/Main_NXT.java
@@ -1,10 +1,15 @@
 import lejos.nxt.Button;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
 
 public class Main_NXT
 {
 
 	public static void main(String[] args)
 	{
+		TouchSensor touch = new TouchSensor(SensorPort.S1);
+		while(!touch.isPressed());
+
 		System.out.println("test");
 		System.out.println("druk op een knop");
 
