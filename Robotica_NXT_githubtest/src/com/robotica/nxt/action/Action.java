882bb378c852feebb8242fd38cbaa5793bb2018d
GSamuel <gideon_hoog@hotmail.com>
2014-09-23 23:18:09 +0200

    deleted the 2 github test projects

--- Robotica_NXT_githubtest/src/com/robotica/nxt/action/Action.java
+++
@@ -1,6 +0,0 @@
-package com.robotica.nxt.action;
-
-public interface Action
-{
-	public void execute();  
-}
--- Robotica_PC_githubtest/src/GithubTestClass.java
+++
@@ -1,7 +0,0 @@
-
-//comment line lokaal
-public class GithubTestClass
-{
-	//yay dit is leuk
-	//Deze comment is lokaal server gemaakt
-}
