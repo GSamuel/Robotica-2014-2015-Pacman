b72e5b7748c31681260c940333f9a3f619d6d035
GSamuel <gideon_hoog@hotmail.com>
2014-09-23 11:41:58 +0200

    made a simple webcam selector

--- Robotica_2014_Pacman_PC/src/MainTest.java
+++ Robotica_2014_Pacman_PC/src/MainTest.java
@@ -1,8 +1,3 @@
-import javax.swing.JFrame;
-
-import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamPanel;
-import com.github.sarxos.webcam.WebcamResolution;
 
 public class MainTest
 {
