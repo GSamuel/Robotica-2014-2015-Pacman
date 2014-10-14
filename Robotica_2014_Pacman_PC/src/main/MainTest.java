adef8c2bab77ca513cb67a617cf7e32e82928575
GSamuel <gideon_hoog@hotmail.com>
2014-10-14 11:27:00 +0200

    relocating some files

---
+++ Robotica_2014_Pacman_PC/src/main/MainTest.java
@@ -0,0 +1,28 @@
+package main;
+import com.robotica.pc.model.Entity;
+import com.robotica.pc.model.EntityList;
+import com.robotica.pc.model.EntityType;
+import com.robotica.pc.model.Maze;
+import com.robotica.pc.model.Tile;
+import com.robotica.pc.model.World;
+
+public class MainTest
+{
+
+	public static void main(String[] args) throws InterruptedException
+	{		
+		Entity[] entities = new Entity[3];
+		entities[0] = new Entity(EntityType.PACMAN, 0);
+		entities[1] = new Entity(EntityType.GHOST, 1);
+		entities[2] = new Entity(EntityType.GHOST, 2);
+		EntityList list = new EntityList(entities);
+
+		Maze m = new Maze(10, 10);
+		m.setTile(2, 2, Tile.WALL);
+		m.setTile(3, 2, Tile.WALL);
+		m.show();
+
+		World w = new World(m, list);
+	}
+
+}
