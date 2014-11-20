d5b7b8d746d94f4ae1b41cfb871204d5469c422b
GSamuel <gideon_hoog@hotmail.com>
2014-11-20 14:12:31 +0100

    connectedEntity heeft nu een connect knop in de gui

--- Robotica_2014_Pacman_PC/src/com/robotica/pc/gui/ConnectedEntityPanel.java
+++ Robotica_2014_Pacman_PC/src/com/robotica/pc/gui/ConnectedEntityPanel.java
@@ -4,25 +4,37 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import com.robotica.pc.model.ConnectedEntity;
 
-public class ConnectedEntityPanel extends JPanel implements Observer
+public class ConnectedEntityPanel extends JPanel implements Observer, ActionListener
 {
 	private static final long serialVersionUID = -1358499148193705488L;
 	private ConnectedEntity entity;
 	
 	private static final int FONT_SIZE = 20;
 	private int count = 0;
+	
+	private JButton connectButton;
 
 	public ConnectedEntityPanel(ConnectedEntity entity)
 	{
 		this.entity = entity;
 		this.entity.addObserver(this);
+		
+		connectButton = new JButton("connect");
+		connectButton.setLocation(50, 100);
+		connectButton.addActionListener(this);
+		this.add(connectButton);
+		this.revalidate();
+		
 		setPreferredSize(new Dimension(300,200));
 	}
 	
@@ -33,7 +45,7 @@ public class ConnectedEntityPanel extends JPanel implements Observer
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, FONT_SIZE));
 		
-		count = 0;
+		count = 2;
 
 		drawString(g2d, "devName:"+entity.getDeviceName());
 		drawString(g2d, "connected:"+Boolean.toString(entity.isConnected()));
@@ -52,6 +64,12 @@ public class ConnectedEntityPanel extends JPanel implements Observer
 	public void update(Observable arg0, Object arg1)
 	{
 		repaint();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0)
+	{
+		entity.connect();
 	}
 
 }
